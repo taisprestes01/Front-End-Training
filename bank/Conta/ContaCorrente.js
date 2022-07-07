35137a42f64f09aa7071085e0f91b013bce18492
TAIS BORGES <taisprestes01@gmail.com>
2022-07-06 23:24:53 -0300

    bank

--- bank/Conta/ContaCorrente.js
+++ bank/Conta/ContaCorrente.js
@@ -1,15 +1,15 @@
-import { Conta } from "./Conta.js"; 
+import { Conta } from "./Conta.js";
+
 export class ContaCorrente extends Conta{
     static numeroDeContas = 0;
-    constructor(cliente, agencia){
-        super(0,cliente,agencia);
-        /*this.agencia = agencia;
-        this.cliente = cliente; Inves de declarar de novo o mesmo cliente conta, chamar da conta com super*/
-        ContaCorrente.numeroDeContas += 1;//uma variavel static para conseguir usar em todas as classes
+    constructor(cliente, agencia) {
+        super(0,cliente, agencia);
+        ContaCorrente.numeroDeContas += 1;
     }
-      //Sobreescrevendo o metodo sacar de Conta
-    sacar(valor){
+    
+    //sobreescrevendo o comportamento de sacar
+    sacar(valor) {
         let taxa = 1.1;
         return this._sacar(valor, taxa);
     }
-  }
+}
